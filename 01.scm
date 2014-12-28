11909696267be6aabbddc990ec5c40afe6d807e7
Ghufran Syed <ghufran.syed@gmail.com>
2014-12-27 17:49:00 -0800

    ex 1.6

--- 01.scm
+++ 01.scm
@@ -51,6 +51,37 @@ the resulting operator primitive (+ or -) is applied to each of
 the remaining arguments
 |#
 
+#| 1.5
+With applicative-order ('inside-out'), the program will give an 
+error or enter a permanent loop, because it will attempt to evaluate p
+, which evaluates to p
+With normal-order ('outside-in'), the expression will return 0, and the 
+expression for y ( '(p)) will never be evaluated
+
+in drracket, the interpreter gets stuck in a loop, indicating that applicative-order
+is being used
+|#
+
+(define (square x)
+  (* x x))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+
+(define (average x y) 
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
 
 
 
